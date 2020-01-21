db47952fde5a966355dca6586698cb70e238bcf6
Joey Petrone <joey.petrone@gmail.com>
2020-01-20 19:40:00 -0600

    finished strings

---
+++ objects.js
@@ -0,0 +1,21 @@
+console.log('objects');
+
+// return length of word
+const wordLength = (word) => {
+    return `Number of letters is ${word.length}`;
+};
+
+console.log(wordLength('cat'));  // 3
+console.log(wordLength('monkeybutt'));  // 10
+
+// Check your age
+const ageCheck = (age) => {
+    if (age >= 21) {
+        console.log('PARTY!!!!!!!!!!!!!!!!!!!!!');
+    } else {
+        console.log('No drinks for you!')
+    }
+};
+
+ageCheck(12); // No drinks for you!
+ageCheck(24); // PARTY!!!!!!!!!!!!!!!!!!!!!
