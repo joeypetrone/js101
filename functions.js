fcf0065d379e412c762f4c262fcd8711ce58b5b2
Joey Petrone <joey.petrone@gmail.com>
2020-01-20 18:47:54 -0600

    wrote nuggetizer function

---
+++ functions.js
@@ -0,0 +1,12 @@
+console.log('Functions')
+
+const nuggetizer = (animal) => {
+    const output = `processed ${animal}`;
+    return output;
+};
+
+console.log(nuggetizer('chicken'));
+console.log(nuggetizer('pig'));
+console.log(nuggetizer('cow'));
+console.log(nuggetizer('fish'));
+console.log(nuggetizer('tire'));
