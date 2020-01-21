4ebe354f76332ad3e77a74788eae31961dbda9d9
Joey Petrone <joey.petrone@gmail.com>
2020-01-20 20:44:16 -0600

    Added objects challenges

--- objects.js
+++ objects.js
@@ -19,3 +19,38 @@ const ageCheck = (age) => {
 
 ageCheck(12); // No drinks for you!
 ageCheck(24); // PARTY!!!!!!!!!!!!!!!!!!!!!
+
+const expense = {
+    dateCreated: '01/17/2020',
+    location: 'Baja Burrito',
+    cost: 12.57,
+    isGood: true
+};
+
+// dot notation
+console.log(expense.cost);
+
+// bracket notation
+console.log(expense['cost']);
+
+const employee = {
+    firstName: 'Bob',
+    lastName: 'Newman',
+    job: 'Engineer',
+    salary: 75000.00,
+    'favorite drink': 'Diet Coke',
+    isGoodAtJob: true
+};
+
+// Challenge
+
+const statusMaker = (emp) => {
+    if (emp.firstName[0] === 'M') {
+        emp.status = 'vip';
+    } else {
+        emp.status = 'peasant';
+    }
+    return emp
+}
+
+console.log(statusMaker(employee));
