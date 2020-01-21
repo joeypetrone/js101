6e011810f43fb67b819d43dfc46fe041c00abd6c
Joey Petrone <joey.petrone@gmail.com>
2020-01-21 00:07:10 -0600

    Combo Challenges, except palindrome

--- combo.js
+++ combo.js
@@ -7,24 +7,50 @@ console.log('Combos');
 // Challenge #1
 
 const evenOdd = (num) => {
-    if (num % 2) {
+    if (!Number.isInteger(num)){
+        return `"${num}" is not a number.`;
+    } else if (num % 2) {         // returns true for an odd number due to it having a 1 remainder from a modulus 2 
         return 'Odd';
-    } else {
+    } else if (num != 0) {
         return 'Even';
+    } else {
+        return 'Zero';
     }
 };
 
-console.log(evenOdd(33));
+console.log(evenOdd(33)); // Odd
+console.log(evenOdd(34)); // Even
+console.log(evenOdd(-1)); // Odd
+console.log(evenOdd(0)); // Zero
+console.log(evenOdd('Hello')); // "Hello" is not a number.
+
+
+
 
 // Challenge #2
 // given a number, find its opposite. 2 = -2
 
 // Challenge #2
 
-const findOpp = (int) => {
-    if (int > -int) 
-
+const findOpp = (num) => {
+    if (!Number.isInteger(num)){
+        return `${num} is not a number.`;        
+    } else if (num !== 0) {
+        return num = -1*num;
+    } else {
+        return 'No opposite for 0';
+    }
 };
+
+console.log(findOpp(2)); // -2
+console.log(findOpp(-2)); // 2
+console.log(findOpp(0)); // No opposite for 0
+console.log(findOpp('Two')); // This is not an number
+console.log(findOpp(2.0)); // -2.0
+const num = 2.0; // -2.0
+console.log(num);
+
+
 
 // Challenge #3
 // A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
@@ -42,5 +68,18 @@ const palindrome = (pal) => {
 // Given a string with the animal name, that Lawrence encounters, output small if the animal is an alligator (case insensitive) otherwise return wide.
 
 // Challenge #4
+ const newCreature = (animal) => {
+     const isGator = 'alligator'; 
+     if (animal.toLowerCase().includes(isGator) && animal.length === isGator.length) {
+         return 'small';
+     } else {
+         return 'wide';
+     }
+ }
+
+console.log(newCreature('Alligator')); // small
+console.log(newCreature('alligator')); // small
+console.log(newCreature('bird')); // wide
+console.log(newCreature('frog')); // wide
 
 
